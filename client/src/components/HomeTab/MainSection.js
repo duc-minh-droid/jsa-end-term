e71d5b81f7c9e1d2319803df4ca4d960b7ca4520
duc-minh-droid <ducminhcsp@gmail.com>
2022-06-29 14:00:19 +0700

    got so many bugs to fix

--- client/src/components/HomeTab/MainSection.js
+++ client/src/components/HomeTab/MainSection.js
@@ -6,29 +6,29 @@ import rec from '../../assets/image/Home/rectangle2.jpg'
 function MainSection() {
   return (
     <div>
-        <div className="main-section first-section">
-            <div className="main-desc">
-                <p className="main-title">Listening is everything</p>
-                <p>Millions of songs and podcasts.</p>
+            <div className="main-section first-section">
+                <div className="main-desc">
+                    <p className="main-title">Listening is everything</p>
+                    <p>Millions of songs and podcasts.</p>
+                </div>
             </div>
-        </div>
 
-        <div className="main-section second-section">
-            <div className="second-desc">
-                <p className="main-title">Top songs being discovered around the world right now</p>
-                <IntroVideo />
+            <div className="main-section second-section">
+                <div className="second-desc">
+                    <p className="main-title">Top songs being discovered around the world right now</p>
+                    <IntroVideo />
+                </div>
             </div>
-        </div>
 
-        <div className="main-section third-section">
-            <div className="third-desc">
-                <p className="main-title">Open the world of music</p>
-                <p>Intuitive. Powerful. Runs everywhere</p>
+            <div className="main-section third-section">
+                <div className="third-desc">
+                    <p className="main-title">Open the world of music</p>
+                    <p>Intuitive. Powerful. Runs everywhere</p>
+                </div>
+                <div className="image-container">
+                    <img src={rec} alt='' style={{height:'500px', width:'800px'}}/>
+                </div>
             </div>
-            <div className="image-container">
-                <img src={rec} alt='' style={{height:'500px', width:'800px'}}/>
-            </div>
-        </div>
     </div>
   )
 }
